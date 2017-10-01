86c25c05b41f955717ee93d6c2c053ed674655fa
sean0923 <iamseanhong@gmail.com>
2017-10-01 02:00:57 -0700

    Box setup for Sean and Jerry

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -8,7 +8,16 @@ import FloorsPanel from './components/FloorsPanel.jsx';
 import BeforeImage from './components/BeforeImage.jsx';
 import SpaceThreshold from './components/SpaceThreshold.jsx';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import ImageBigBox from './components/ImageBigBox.jsx';
 // import AfterImage from './components/AfterImage.jsx';
+
+const seanBigBox = {
+  marginRight: '2%',
+  marginLeft: '2%',
+  width: '96%',
+  border: '1px dotted red',
+  height: '600px'
+}
 
 class App extends React.Component {
   constructor(props) {
@@ -38,6 +47,9 @@ class App extends React.Component {
         <div>
           <h1>Item List</h1>
           <List items={this.state.items}/>
+          <div style={seanBigBox}>
+            <ImageBigBox />
+          </div>
           <Elevator/>
           <UpAndDownPanel />
           <FloorsPanel />
