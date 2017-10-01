7daa629b85c2d88d3f1e3bd0a9f3069836d77807
Jonathan Kim <jykim16@gmail.com>
2017-10-01 09:30:36 -0700

    server connect

--- react-client/src/components/DemoContent.jsx
+++ react-client/src/components/DemoContent.jsx
@@ -63,7 +63,7 @@ class DemoContent extends React.Component {
     return (
       <div style={styles.wrapper}>
         <div style={styles.one}><img src={this.props.img1} style={picStyle}/></div>
-        <div style={styles.two}><img src={this.props.imgDiff} style={picStyle}/></div>
+        <div style={styles.two}><img src={'data:image/png;base64,'+this.props.diffImg} style={picStyle}/></div>
         <div style={styles.three}><img src={this.props.img2} style={picStyle}/></div>
         <div style={styles.four}>
           <SpaceThreshold percentOccupied={this.props.percentOccupied} />
